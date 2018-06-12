3f6961c3aade099cff63e1bdb7b91dd59100947f
Poga Po <poga.bahamut@gmail.com>
2018-06-12 22:36:55 +0800

    add overwork api, transformed workhour

--- index.js
+++ index.js
@@ -23,6 +23,58 @@ router.post('/validate', async function (ctx) {
   ctx.body = JSON.stringify(result)
 })
 
+router.post('/validate_two_week', async function (ctx) {
+  if (!ctx.request.body.schedule) {
+    ctx.response.status = 406
+    ctx.body = 'Invalid schedule data'
+    return
+  }
+
+  let schedule = shift.Schedule.fromTime(ctx.request.body.schedule)
+
+  let result = shift.validate(schedule, {transformed: shift.validate.two_week})
+  ctx.body = JSON.stringify(result)
+})
+
+router.post('/validate_four_week', async function (ctx) {
+  if (!ctx.request.body.schedule) {
+    ctx.response.status = 406
+    ctx.body = 'Invalid schedule data'
+    return
+  }
+
+  let schedule = shift.Schedule.fromTime(ctx.request.body.schedule)
+
+  let result = shift.validate(schedule, {transformed: shift.validate.four_week})
+  ctx.body = JSON.stringify(result)
+})
+
+router.post('/validate_eight_week', async function (ctx) {
+  if (!ctx.request.body.schedule) {
+    ctx.response.status = 406
+    ctx.body = 'Invalid schedule data'
+    return
+  }
+
+  let schedule = shift.Schedule.fromTime(ctx.request.body.schedule)
+
+  let result = shift.validate(schedule, {transformed: shift.validate.eight_week})
+  ctx.body = JSON.stringify(result)
+})
+
+router.post('/overwork', async function (ctx) {
+  if (!ctx.request.body.schedule) {
+    ctx.response.status = 406
+    ctx.body = 'Invalid schedule data'
+    return
+  }
+
+  let schedule = shift.Schedule.fromTime(ctx.request.body.schedule)
+
+  let result = shift.overwork.check(schedule)
+  ctx.body = JSON.stringify(result)
+})
+
 app
   .use(logger())
   .use(bodyParser())
